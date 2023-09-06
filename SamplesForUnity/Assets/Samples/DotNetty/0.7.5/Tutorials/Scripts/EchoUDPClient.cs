9dd4c06964d434c88bf913ca0af00511c9c77e70
Clark <yangpichen@gmail.com>
2023-09-06 12:06:54 +0800

    v0.7.6

--- SamplesForUnity/Assets/Samples/DotNetty/0.7.5/Tutorials/Scripts/EchoUDPClient.cs
+++ SamplesForUnity/Assets/Samples/DotNetty/0.7.5/Tutorials/Scripts/EchoUDPClient.cs
@@ -1,17 +1,17 @@
-﻿using DotNetty.Codecs;
-using DotNetty.Handlers.Logging;
+﻿using DotNetty.Handlers.Logging;
 using DotNetty.Handlers.Tls;
 using DotNetty.Transport.Bootstrapping;
 using DotNetty.Transport.Channels;
 using DotNetty.Transport.Channels.Sockets;
 using System;
 using System.Net.Security;
+using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace Echo.Client
 {
-    public class EchoClient
+    public class EchoUDPClient
     {
         private bool isSsl = false;
         private IChannel channel;
@@ -19,7 +19,7 @@ namespace Echo.Client
         public bool Connected { get; private set; }
         public async Task<IChannel> ConnectAsync(string host, int port)
         {
-            var group = new MultithreadEventLoopGroup();
+            var group = new MultithreadEventLoopGroup(1);
             X509Certificate2 cert = null;
             string targetHost = null;
             if (isSsl)
@@ -32,9 +32,9 @@ namespace Echo.Client
                 var bootstrap = new Bootstrap();
                 bootstrap
                     .Group(group)
-                    .Channel<TcpSocketChannel>()
-                    .Option(ChannelOption.TcpNodelay, true)
-                    .Handler(new ActionChannelInitializer<ISocketChannel>(c =>
+                    //.Channel<SocketDatagramChannel>()
+                    .ChannelFactory(()=>new SocketDatagramChannel(AddressFamily.InterNetwork))
+                    .Handler(new ActionChannelInitializer<IChannel>(c =>
                     {
                         IChannelPipeline pipeline = c.Pipeline;
 
@@ -43,19 +43,19 @@ namespace Echo.Client
                             pipeline.AddLast("tls", new TlsHandler(stream => new SslStream(stream, true, (sender, certificate, chain, errors) => true), new ClientTlsSettings(targetHost)));
                         }
                         pipeline.AddLast(new LoggingHandler("CLIENT-CONN"));
-                        pipeline.AddLast("framing-enc", new LengthFieldPrepender(2));
-                        pipeline.AddLast("framing-dec", new LengthFieldBasedFrameDecoder(ushort.MaxValue, 0, 2, 0, 2));
+                        //pipeline.AddLast("framing-enc", new LengthFieldPrepender(2));
+                        //pipeline.AddLast("framing-dec", new LengthFieldBasedFrameDecoder(ushort.MaxValue, 0, 2, 0, 2));
 
-                        pipeline.AddLast("echo", new EchoClientHandler());
+                        pipeline.AddLast("echo", new EchoUDPClientHandler());
                     }));
 
-                channel = await bootstrap.ConnectAsync(host, port);
+                channel = await bootstrap.ConnectAsync(host, port).ConfigureAwait(false);
                 this.Connected = true;
                 _ = channel.CloseCompletion.ContinueWith(async (t) =>
-                 {
-                     if (group != null)
-                         await group.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1));
-                 });
+                {
+                    if (group != null)
+                        await group.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+                });
                 return channel;
             }
             catch (Exception)
@@ -63,12 +63,12 @@ namespace Echo.Client
                 try
                 {
                     if (channel != null)
-                        await channel.CloseAsync();
+                        await channel.CloseAsync().ConfigureAwait(false);
                 }
                 catch (Exception) { }
 
                 if (group != null)
-                    await group.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1));
+                    await group.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1)).ConfigureAwait(false);
 
                 this.Connected = false;
                 throw;
@@ -78,7 +78,7 @@ namespace Echo.Client
         public async Task DisconnectAsync()
         {
             if (this.channel != null)
-                await channel.DisconnectAsync();
+                await channel.DisconnectAsync().ConfigureAwait(false);
             this.Connected = false;
         }
     }
---
+++ SamplesForUnity/Assets/Samples/DotNetty/0.7.5/Tutorials/Scripts/UDPLauncher.cs
@@ -0,0 +1,80 @@
+﻿using DotNetty.Buffers;
+using DotNetty.Transport.Channels;
+using DotNetty.Unity;
+using Echo.Client;
+using Echo.Server;
+using System;
+using System.Text;
+using UnityEngine;
+
+public class UDPLauncher : MonoBehaviour
+{
+    EchoUDPServer server;
+    //EchoClient client;
+    EchoUDPClient client;
+    IChannel channel;
+    //private string host = "127.0.0.1";
+    //private int port = 8007;
+    //private string host = "127.0.0.1";
+    private string host = "192.168.1.253";
+    private int port = 8800;
+    void Start()
+    {
+        UnityLoggerFactory.Default.Level = Level.DEBUG;
+
+        server = new EchoUDPServer(port);
+        client = new EchoUDPClient();
+
+        Environment.SetEnvironmentVariable("io.netty.noPreferDirect", "false");
+    }
+
+
+    void OnGUI()
+    {
+        int x = 50;
+        int y = 50;
+        int width = 200;
+        int height = 100;
+        int i = 0;
+        int padding = 10;
+
+        GUI.skin.button.fontSize = 25;
+
+        if (GUI.Button(new Rect(x, y + i++ * (height + padding), width, height), server.Started ? "Stop Server" : "Start Server"))
+        {
+            if (server.Started)
+                _ = server.Stop();
+            else
+                _ = server.Start();
+        }
+
+        if (GUI.Button(new Rect(x, y + i++ * (height + padding), width, height), client.Connected ? "Disconnect" : "Connect"))
+        {
+            if (client.Connected)
+                _ = client.DisconnectAsync();
+            else
+                Connect();
+        }
+
+        if (GUI.Button(new Rect(x, y + i++ * (height + padding), width, height), "Send Message"))
+        {
+            IByteBuffer message = Unpooled.Buffer(256);
+            message.WriteBytes(Encoding.UTF8.GetBytes("this is a request."));
+            this.channel.WriteAndFlushAsync(message);
+        }
+    }
+
+    private async void Connect()
+    {
+        channel = await client.ConnectAsync(host, port);
+    }
+
+    private void OnDestroy()
+    {
+        if (server != null && server.Started)
+            _ = server.Stop();
+
+        if (client != null && client.Connected)
+            _ = client.DisconnectAsync();
+    }
+}
